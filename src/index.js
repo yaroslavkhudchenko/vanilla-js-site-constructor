351eff8b35e0e89f07b7874d88d98d36d48a2431
Yari PL <yaroslav.khudchenko@gmail.com>
2020-08-21 19:52:04 +0200

    loop model to get correct html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import { model } from './model';
+
 import './styles/app.scss';
+
+
+model.forEach(block => {
+    
+    let html = '';
+    
+    if(block.type = 'title') {
+        html = blockTitle()
+    } else if (block.type = 'text') {
+        html = blockText()
+    } else if (block.type = 'textColumns') {
+        htmk = blockColumns()
+    }
+
+})
