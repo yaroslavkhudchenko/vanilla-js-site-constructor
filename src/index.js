b3fea676940ded40b6623fe8079d8986bd42e158
Yari PL <yaroslav.khudchenko@gmail.com>
2020-08-21 20:40:38 +0200

    add functions to change each html block render

--- src/index.js
+++ src/index.js
@@ -2,17 +2,49 @@ import { model } from './model';
 
 import './styles/app.scss';
 
+const previewDiv = document.querySelector('.configurator__preview');
+
+const blockTitle = (block) => {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <h1>${block.value}</h1>
+      </div>
+    </div>
+  `;
+};
+
+const blockText = (block) => {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <p>${block.value}</p>
+      </div>
+    </div>
+  `;
+};
+
+const blockTextColumns = (block) => {
+  const html = block.value.map((item) => `<div class="col-sm">${item}</div>`);
+  return `
+    <div class="row">
+      ${html.join("")}
+    </div>
+  `;
+};
+
+
 
 model.forEach(block => {
     
     let html = '';
     
-    if(block.type = 'title') {
-        html = blockTitle()
-    } else if (block.type = 'text') {
-        html = blockText()
-    } else if (block.type = 'textColumns') {
-        htmk = blockColumns()
+    if(block.type === 'title') {
+        html = blockTitle(block)
+    } else if (block.type === 'text') {
+        html = blockText(block)
+    } else if (block.type === 'textColumns') {
+        html = blockTextColumns(block);
     }
-
+    previewDiv.insertAdjacentHTML('beforeend', html)
 })
