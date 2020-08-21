d5670931c0b07de345257dceda51831e34aa2427
Yari PL <yaroslav.khudchenko@gmail.com>
2020-08-21 19:46:00 +0200

    created basic model file

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+    { type: "title", value: "defaultTitle", options: {} },
+    { type: "text", value: 'lorem ipsum dolor sit amet, consecteture wwjgawjgjawg jgwajg aw', options: {} },
+    { type: "textColumns", value: [
+        '1 example',
+        '2 example',
+        '3 example'
+    ], options: {} },
+];
