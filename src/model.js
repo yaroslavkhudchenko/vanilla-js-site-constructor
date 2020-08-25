c102d555a1db420a2c8afc45b06074545b317b34
Yari PL <yaroslav.khudchenko@gmail.com>
2020-08-25 03:34:51 +0200

    style a bit

--- src/model.js
+++ src/model.js
@@ -3,9 +3,10 @@ import img1 from "./assets/img/1.png";
 export const model = [
          {
            type: "title",
-           value: "defaultTitle",
+           value: "Page constructor",
            options: {
-             styles: "background: navy; color:#fff; text-align:center",
+             styles:
+               "background: antiquewhite; color:#000; text-align:center;padding:20px 0;font-size:30px;",
              tag: "h2",
            },
          },
@@ -14,14 +15,16 @@ export const model = [
            value:
              "lorem ipsum dolor sit amet, consecteture wwjgawjgjawg jgwajg aw",
            options: {
-             styles: "background: red; color:yellow",
+             styles:
+               "background: antiquewhite; color:#000; padding: 20px 0; padding-left:15px;font-size:25px;",
            },
          },
          {
            type: "textColumns",
            value: ["1 example", "2 example", "3 example"],
            options: {
-             styles: "background: yellow; color:green; padding:5px",
+             styles:
+               "background: antiquewhite; color:#000; padding: 50px 15px;font-size: 20px;",
            },
          },
          {
@@ -29,6 +32,8 @@ export const model = [
            value: img1,
            options: {
              alt: "img1",
+             styles:
+               "background: antiquewhite; padding:40px 0; padding-left:15px",
            },
          },
        ];
--- src/templateFunctions.js
+++ src/templateFunctions.js
@@ -13,6 +13,6 @@ const text = (block) => row(col(`<p>${block.value}</p>`), block.options.styles);
 
 const textColumns = (block) => row(block.value.map((item) => col(item)).join(""), block.options.styles);
 
-const image = (block) => row(col(`<img src='${block.value}' alt='${block.options.alt}' />`))
+const image = (block) => row(col(`<img src='${block.value}' alt='${block.options.alt}' />`), block.options.styles)
 
 export const templates = { title, text, textColumns, image };
